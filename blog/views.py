8427d6e1338ea74c00a720cb7d27aa08f5c43e73
Michael Zapata <mikeyfriends@hotmail.com>
2019-11-04 22:49:27 +0000

    added blog html

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
+#import the models so you can use the data stored in this table/model
+from .models import Blog
+
 # Create your views here.
+def allblogs(request):
+    #return all blogs
+    blogs = Blog.objects
+    return render(request, 'blog/allblogs.html', {'blogs':blogs})
